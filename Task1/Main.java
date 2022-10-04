b5c42d58d5a08bf7a0c0493c2b030eb07102fb14
Andriy0708 <andriy.borys21@gmail.com>
2022-10-04 22:54:07 +0300

    Task1 and Task2

---
+++ Task1/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        int i = 5;
+        int b = 3;
+        {
+            if (i > b) ;
+
+
+            System.out.println(5);
+
+
+        }
+
+    }
+}
+
+
---
+++ Task2/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        int a= 1;
+        int b=2;
+        int c =3;
+        int d=4;
+        int e=5;
+        int h=6;
+        int g=7;
+        int j=8;
+        int w=9;
+        int n=10;
+        int sum =0;
+        {
+            for (int i = 0; i < 11; i++)
+                sum += i;
+
+
+            System.out.println(sum);
+
+
+        }
+            }
+
+}
+
